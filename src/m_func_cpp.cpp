f6366153878f7e0443f8e7b9c0e3f3e42a6631cd
pcollender <pcollender@berkeley.edu>
2019-05-11 21:56:11 -0700

    ...

--- src/m_func_cpp.cpp
+++ src/m_func_cpp.cpp
@@ -53,8 +53,9 @@ arma::mat indexing(const std::vector<arma::vec> s, const int l1, const int l2,
           index_out.set_size(temp0.n_elem * temp1.n_elem, 2);
           for(i = 0; i < temp0.n_elem; i++){
             for(j = 0; j < temp1.n_elem; j++){
-              index_out(rowcount,0) = min(temp0[i], temp1[j]);
-              index_out(rowcount,1) = max(temp0[i], temp1[j]);
+              int i_ = temp0[i]; int j_ = temp1[j];
+              index_out(rowcount,0) = min(i_, j_);
+              index_out(rowcount,1) = max(i_, j_);
               rowcount++;
             }
           }
@@ -88,10 +89,9 @@ std::vector<arma::vec> indexing_na(const std::vector<arma::vec> s,
   // Subset
   arma::uvec s0_l1 = s0 > l1;
   
-  if(dedupe == true){
-    arma::uvec s0_l2 = s0 <= min(l2,l4-1); //double constraint for upper triangular dedup matrix
-  }else{
-    arma::uvec s0_l2 = s0 <= l2;
+  arma::uvec s0_l2 = s0 <= l2;
+  if(dedupe == true && (l4-1) < l2){
+    s0_l2 = s0 <= (l4-1); //double constraint for upper triangular dedup matrix
   }
   
   arma::uvec s1_l3 = s1 > l3;
@@ -189,7 +189,7 @@ arma::vec getNotIn(const arma::vec vec1, const arma::vec vec2){
 }
 
 std::vector<SpMat> create_sparse_na(const std::vector< std::vector<arma::vec> > nas,
-				    const arma::vec dims, const arma::vec lowerlims, const <bool> dedupe){
+				    const arma::vec dims, const arma::vec lowerlims, const bool dedupe){
 //lowerlims is the lower index of each dimension
 
   int i; int j; int k; double val; const int nobs_a = dims[0]; const int nobs_b = dims[1];
@@ -213,7 +213,7 @@ std::vector<SpMat> create_sparse_na(const std::vector< std::vector<arma::vec> > 
     // Create triplet
     std::vector<Trip> tripletList;
     
-    if(dedupe == true){
+    if(dedupe){
       
       
       tripletList.reserve(nas_a.size() * nobs_b + nas_b.size() * nobs_a);
@@ -227,7 +227,7 @@ std::vector<SpMat> create_sparse_na(const std::vector< std::vector<arma::vec> > 
       //j_hat = i_hat + lowerlim[0]-lowerlim[1] + 1: end  
       
       for(j = 0; j < nas_a.size(); j++){
-        for(k = max(0,nas_a[j] + lowerlims(0) - lowerlims(1)); k < nobs_b; k++){
+        for(k = max(0.0,nas_a[j] + lowerlims(0) - lowerlims(1)); k < nobs_b; k++){
           tripletList.push_back(Trip(nas_a[j]-1, k, val));
         }
       }
