dd66f3f67dfd06287806e2fc60a4e8430848ec97
pcollender <pcollender@berkeley.edu>
2019-05-13 13:35:12 -0700

    ...

--- R/tableCounts.R
+++ R/tableCounts.R
@@ -82,7 +82,7 @@ tableCounts <- function(gammalist, nobs.a, nobs.b, dedupe = FALSE, n.cores = NUL
     ind <- as.matrix(expand.grid.jc(ind.i, ind.j))
 
     if(dedupe)
-      ind = ind[which(ind[,1]<=ind[,2]),] #removing empty blocks for internal linkage represented by upper right tri matrix
+      ind = as.matrix(ind[which(ind[,1]<=ind[,2]),],ncol = 2) #removing empty blocks for internal linkage represented by upper right tri matrix
     #then number of excess zeroes should be n.lim.1^2/2 + n.lim.1/2 for all entries where ind[,1] = ind[,2]
     
     ## Run main function
