7cbc9ce5d31b3b7bd562df91c5f42d8ca9699881
pcollender <pcollender@berkeley.edu>
2019-06-12 15:09:43 -0700

    fixed getMatches() behavior for internal linkage and deduplication, and expanded documentation

--- R/getMatches.R
+++ R/getMatches.R
@@ -4,7 +4,10 @@ globalVariables(c('V2', 'V3', '.'))
 #'
 #' Subset two data frames to the matches returned by \code{fastLink()}
 #' or \code{matchesLink()}. Can also return a single deduped data frame
-#' if dfA and dfB are identical and fl.out is of class 'fastLink.dedupe'.
+#' if dfA and dfB are identical and fl.out is of class 'fastLink.dedupe'. 
+#' The deduped data frame contains an extra column, \code{prior.indices}, 
+#' which contains NA values for all rows not identified as duplicates, and otherwise 
+#' lists the row indices of all prior rows linked to the current row. 
 #'
 #' @usage getMatches(dfA, dfB, fl.out, threshold.match, combine.dfs)
 #' @param dfA Dataset A - matched to Dataset B by \code{fastLink()}.
@@ -13,8 +16,8 @@ globalVariables(c('V2', 'V3', '.'))
 #' @param threshold.match A number between 0 and 1 indicating the lower bound that the
 #' user wants to declare a match. For instance, threshold.match = .85 will return all pairs with posterior probability greater than .85 as matches.
 #' Default is 0.85.
-#' @param combine.dfs Whether to combine the two data frames being merged into a single data frame. If FALSE, two data frames are returned in a list. Default is TRUE.
-#' @param twolineformat Boolean. Whether to  return a single dataframe with each matched pair presented in subsequent rows, with spaces separating the pairs. Only works if \code{combine.dfs = FALSE}
+#' @param combine.dfs Whether to combine the two data frames being merged into a single data frame. If FALSE, two data frames are returned in a list. Does nothing in the case of deduping a single dataframe. Default is TRUE.
+#' @param inspect.matches Boolean. Whether to  return a single dataframe with each matched pair presented in subsequent rows, with spaces separating the pairs. Overrides \code{combine.dfs}
 #'
 #' @return \code{getMatches()} returns a list of two data frames:
 #' \item{dfA.match}{A subset of \code{dfA} subsetted down to the successful matches.}
@@ -30,7 +33,7 @@ globalVariables(c('V2', 'V3', '.'))
 #' ret <- getMatches(dfA, dfB, fl.out)
 #' }
 #' @export
-getMatches <- function(dfA, dfB, fl.out, threshold.match = 0.85, combine.dfs = TRUE, twolineformat = FALSE){
+getMatches <- function(dfA, dfB, fl.out, threshold.match = 0.85, combine.dfs = TRUE, inspect.matches = FALSE){
 
     ## Convert data frames
     if(any(class(dfA) %in% c("tbl_df", "data.table"))){
@@ -62,8 +65,29 @@ getMatches <- function(dfA, dfB, fl.out, threshold.match = 0.85, combine.dfs = T
         }else{
             out <- list(dfA.match = dfA.match, dfB.match = dfB.match)
         }
+    }else if(inherits(fl.out,'fastLink') && inspect.matches){
+      dfA.match <- dfA[fl.out$matches$inds.a,]
+      dfB.match <- dfB[fl.out$matches$inds.b,]
+      
+      combinedframe = matrix('',nrow = nrow(dfA.match) * 4, ncol = ncol(dfA.match) + 1 + 'posterior' %in% names(fl.out))
+      namevec = c('row.index',names(dfA.match),'p_match')
+      colnames(combinedframe) = namevec[1:ncol(combinedframe)]
+      
+      prefixes = c('dfA','dfB')
+      if(inherits(fl.out, "fastLink.dedupe")) prefixes[2] = 'dfA'
+      
+      combinedframe[seq(1,nrow(combinedframe)-3,4),1:(ncol(dfA.match)+1)] = as.matrix(cbind(paste0(prefixes[1],'.',fl.out$matches$inds.a),dfA.match))
+      combinedframe[seq(2,nrow(combinedframe)-2,4),1:(ncol(dfA.match)+1)] = as.matrix(cbind(paste0(prefixes[2],'.',fl.out$matches$inds.b),dfB.match))
+      
+      patmat = as.matrix(cbind('agreement pattern:',fl.out$patterns))
+      if('posterior' %in% names(fl.out)){
+        patmat = cbind(patmat, round(fl.out$posterior,4))
+      }
+      
+      combinedframe[seq(3,nrow(combinedframe)-1,4),] = patmat
+      out <- as.data.frame(combinedframe)
     }else if(inherits(fl.out, "fastLink.dedupe")){
-
+        #idea - mark each duplicated row (i.e. with larger index), with the list of earlier row indices linked to it
         ## Get ID
         id_tmp <- data.frame(id = 1:nrow(dfA))
 
@@ -84,18 +108,18 @@ getMatches <- function(dfA, dfB, fl.out, threshold.match = 0.85, combine.dfs = T
         ## Dedupe
         matches[, V3 := pasteT(V2), by = "V1"]    
         ans <- matches[, .(id_2 = unique(V3)), by = "V1"]
-        ans$id_2 <- as.numeric(as.factor(ans$id_2))
+        #ans$id_2 <- as.numeric(as.factor(ans$id_2))
         colnames(ans) <- c("id", "id_2")
 
         ## Merge and output new df
-        out_df <- merge(id_tmp, ans, by = "id")
-        dfA$dedupe.ids <- out_df$id_2
+        out_df <- merge(id_tmp, ans, by = "id",all = T)
+        dfA$prior.indices <- out_df$id_2
         out <- dfA
         
         
     }else{
-        dfA.match <- dfA[fl.out$matches$inds.a,]
-        dfB.match <- dfB[fl.out$matches$inds.b,]
+      dfA.match <- dfA[fl.out$matches$inds.a,]
+      dfB.match <- dfB[fl.out$matches$inds.b,]
         if(combine.dfs){
             names.dfB <- names(dfB.match)[!(names(dfB.match) %in% names(dfA.match))]
             if(length(names.dfB) > 0){
@@ -110,36 +134,15 @@ getMatches <- function(dfA, dfB, fl.out, threshold.match = 0.85, combine.dfs = T
             }
             out <- df.match
         }else{
-          if(!twolineformat){
-            dfA.match <- cbind(dfA.match, fl.out$patterns)
-            dfB.match <- cbind(dfB.match, fl.out$patterns)
-            if("posterior" %in% names(fl.out)){
-              dfA.match$posterior <- fl.out$posterior
-              dfB.match$posterior <- fl.out$posterior
-              dfA.match <- dfA.match[dfA.match$posterior >= threshold.match,]
-              dfB.match <- dfB.match[dfB.match$posterior >= threshold.match,]
-            }
-            out <- list(dfA.match = dfA.match, dfB.match = dfB.match)
-          }else{
-            combinedframe = matrix('',nrow = nrow(dfA.match) * 4, ncol = ncol(dfA.match) + 1 + 'posterior' %in% names(fl.out))
-            namevec = c('row.index',names(dfA.match),'p_match')
-            colnames(combinedframe) = namevec[1:ncol(combinedframe)]
-            
-            prefixes = c('dfA','dfB')
-            if(inherits(fl.out, "fastLink.dedupe")) prefixes[2] = 'dfA'
-            
-            combinedframe[seq(1,nrow(combinedframe)-3,4),1:(ncol(dfA.match)+1)] = as.matrix(cbind(paste0(prefixes[1],'.',fl.out$matches$inds.a),dfA.match))
-            combinedframe[seq(2,nrow(combinedframe)-2,4),1:(ncol(dfA.match)+1)] = as.matrix(cbind(paste0(prefixes[2],'.',fl.out$matches$inds.b),dfB.match))
-            
-            patmat = as.matrix(cbind('agreement pattern:',fl.out$patterns))
-            if('posterior' %in% names(fl.out)){
-              patmat = cbind(patmat, round(fl.out$posterior,4))
-            }
-            
-            combinedframe[seq(3,nrow(combinedframe)-1,4),] = patmat
-            out <- as.data.frame(combinedframe)
+          dfA.match <- cbind(dfA.match, fl.out$patterns)
+          dfB.match <- cbind(dfB.match, fl.out$patterns)
+          if("posterior" %in% names(fl.out)){
+            dfA.match$posterior <- fl.out$posterior
+            dfB.match$posterior <- fl.out$posterior
+            dfA.match <- dfA.match[dfA.match$posterior >= threshold.match,]
+            dfB.match <- dfB.match[dfB.match$posterior >= threshold.match,]
           }
-            
+          out <- list(dfA.match = dfA.match, dfB.match = dfB.match)
         }
     }
 
